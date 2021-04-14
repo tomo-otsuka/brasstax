c36111c300f6952511f92b3a73fd98f129d2de6c
Tomo Otsuka <tomo.otsuka@gmail.com>
2021-04-14 00:12:22 -0700

    only allow numbers in text box

--- src/Components.jsx
+++ src/Components.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export function LabeledSelect(props) {
   const selectOptions = props.selectOptions.map((selectOption) => (
     <option value={selectOption.name}>{selectOption.readable}</option>
@@ -24,17 +26,32 @@ export function LabeledCheckbox(props) {
   );
 }
 
-export function LabeledTextBox(props) {
-  return (
-    <div>
-      <label>{props.label + ": "}</label>
-      <input
-        type="text"
-        onChange={props.onChange}
-        disabled={props.disabled}
-      ></input>
-    </div>
-  );
+export class LabeledTextBox extends React.Component {
+  state = { value: "" };
+
+  onChange = (event) => {
+    const value = event.target.value;
+    if (value.match(/[^\d]/)) {
+      this.setState({ value: value.replace(/[^\d]/g, "") });
+      return;
+    }
+    this.setState({ value: value });
+    this.props.onChange(event);
+  };
+
+  render() {
+    return (
+      <div>
+        <label>{this.props.label + ": "}</label>
+        <input
+          type="text"
+          onChange={this.onChange}
+          disabled={this.props.disabled}
+          value={this.state.value}
+        ></input>
+      </div>
+    );
+  }
 }
 
 export function LabeledSpan(props) {
