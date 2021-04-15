18a4009ed301fd8c2e0cfcb6294899ba0561ac86
Tomo Otsuka <tomo.otsuka@gmail.com>
2021-04-14 23:25:03 -0700

    chart updates

--- src/components/TaxChart.jsx
+++ src/components/TaxChart.jsx
@@ -1,153 +1,179 @@
 import React from "react";
 
 import {
-  Chart,
-  LineController,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  BarController,
-  BarElement,
+    Chart,
+    LineController,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarController,
+    BarElement,
+    Tooltip,
+    Title,
 } from "chart.js";
 import {
-  DeductionTypeEnum,
-  FilingStatusEnum,
-  JurisdictionEnum,
+    DeductionTypeEnum,
+    FilingStatusEnum,
+    JurisdictionEnum,
 } from "../constants";
 import {
-  calculateDeduction,
-  calculateIncomeTax,
-  calculateMedicareTax,
-  calculateSocialSecurityTax,
+    calculateDeduction,
+    calculateIncomeTax,
+    calculateMedicareTax,
+    calculateSocialSecurityTax,
 } from "../taxFunctions";
 import { LabeledSelect } from "./Components";
 
 Chart.register(
-  LineController,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  BarController,
-  BarElement
+    LineController,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarController,
+    BarElement,
+    Tooltip,
+    Title
 );
 
 export class TaxChart extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filingStatus: FilingStatusEnum.SINGLE.name,
+    constructor(props) {
+        super(props);
+        this.state = {
+            filingStatus: FilingStatusEnum.SINGLE.name,
 
-      chart: null,
-    };
-  }
-
-  handleStateChange(stateVar, value) {
-    if (!(stateVar in this.state)) {
-      alert(`incorrect state variable: ${stateVar}`);
-      return;
+            chart: null,
+        };
     }
-    this.setState({ [stateVar]: value });
-  }
 
-  chartRef = React.createRef();
+    handleStateChange(stateVar, value) {
+        if (!(stateVar in this.state)) {
+            alert(`incorrect state variable: ${stateVar}`);
+            return;
+        }
+        this.setState({ [stateVar]: value });
+    }
 
-  calculateTax(income) {
-    const jurisdiction = JurisdictionEnum.FEDERAL.name;
-    const filingStatus = this.state.filingStatus;
-    const deductionType = DeductionTypeEnum.STANDARD.name;
+    chartRef = React.createRef();
 
-    const deduction = calculateDeduction(
-      jurisdiction,
-      filingStatus,
-      deductionType,
-      0
-    );
+    calculateTax(income) {
+        const jurisdiction = JurisdictionEnum.FEDERAL.name;
+        const filingStatus = this.state.filingStatus;
+        const deductionType = DeductionTypeEnum.STANDARD.name;
 
-    const taxableIncome = Math.max(0, income - deduction);
+        const deduction = calculateDeduction(
+            jurisdiction,
+            filingStatus,
+            deductionType,
+            0
+        );
 
-    const incomeTax = calculateIncomeTax(
-      jurisdiction,
-      filingStatus,
-      taxableIncome,
-      0,
-      0
-    );
-    const socialSecurityTax = calculateSocialSecurityTax(income);
-    const medicareTax = calculateMedicareTax(filingStatus, income);
+        const taxableIncome = Math.max(0, income - deduction);
 
-    const stateIncomeTax = calculateIncomeTax(
-      JurisdictionEnum.CALIFORNIA.name,
-      filingStatus,
-      taxableIncome,
-      0,
-      0
-    );
-    return incomeTax + socialSecurityTax + medicareTax + stateIncomeTax;
-  }
+        const incomeTax = calculateIncomeTax(
+            jurisdiction,
+            filingStatus,
+            taxableIncome,
+            0,
+            0
+        );
+        const socialSecurityTax = calculateSocialSecurityTax(income);
+        const medicareTax = calculateMedicareTax(filingStatus, income);
 
-  componentDidMount() {
-    const myChartRef = this.chartRef.current.getContext("2d");
+        const stateIncomeTax = calculateIncomeTax(
+            JurisdictionEnum.CALIFORNIA.name,
+            filingStatus,
+            taxableIncome,
+            0,
+            0
+        );
+        return incomeTax + socialSecurityTax + medicareTax + stateIncomeTax;
+    }
 
-    const labels = Array.from({ length: 1000 }, (v, x) => x * 1000);
+    componentDidMount() {
+        const myChartRef = this.chartRef.current.getContext("2d");
 
-    const chart = new Chart(myChartRef, {
-      data: {
-        labels: labels,
-        datasets: [
-          {
-            type: "line",
-            yAxisID: "left-y-axis",
-          },
-          {
-            type: "bar",
-            spanGaps: false,
-            showLine: true,
-            pointRadius: 1,
-            backgroundColor: "rgba(199, 242, 175)",
-            yAxisID: "left-y-axis",
-          },
-        ],
-      },
-      options: {
-        scales: {
-          "left-y-axis": {
-            type: "linear",
-            position: "left",
-          },
-        },
-      },
-    });
+        const labels = Array.from({ length: 1000 }, (v, x) => x * 1000);
 
-    this.setState({ chart: chart });
-  }
+        const chart = new Chart(myChartRef, {
+            data: {
+                labels: labels,
+                datasets: [
+                    {
+                        type: "line",
+                        label: "Effective Tax Rate",
+                        yAxisID: "left-y-axis",
+                        xAxisID: "x-axis"
+                    },
+                    {
+                        type: "bar",
+                        label: "Marginal Tax",
+                        backgroundColor: "rgba(199, 242, 175)",
+                        yAxisID: "left-y-axis",
+                    },
+                ],
+            },
+            options: {
+                scales: {
+                    "left-y-axis": {
+                        type: "linear",
+                        position: "left",
+                        title: {
+                            display: true,
+                            text: "Tax Rate"
+                        },
+                    },
+                    "x-axis": {
+                        title: {
+                            display: true,
+                            text: "Income"
+                        },
+                        ticks: {
+                            autoSkip: false,
+                            callback: function (value, index, values) {
+                                return index % 25 === 0 ? labels[index] : "";
+                            }
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        display: true,
+                        text: "Marginal and Effective Tax Rates"
+                    }
+                }
+            },
+        });
 
-  componentDidUpdate() {
-    const labels = this.state.chart.data.labels;
-    const data = labels.map(
-      (x, i, a) => (this.calculateTax(x) - this.calculateTax(a[i - 1])) / 1000
-    );
-    const effectiveTaxRateData = labels.map(
-      (x, i) => this.calculateTax(x) / labels[i]
-    );
-    this.state.chart.data.datasets[0].data = effectiveTaxRateData;
-    this.state.chart.data.datasets[1].data = data;
-    this.state.chart.update("active");
-  }
+        this.setState({ chart: chart });
+    }
 
-  render() {
-    return (
-      <div className="App App-header">
-        <LabeledSelect
-          onChange={(event) =>
-            this.handleStateChange("filingStatus", event.target.value)
-          }
-          label="Filing Status"
-          selectOptions={Object.values(FilingStatusEnum)}
-        ></LabeledSelect>
-        <canvas id="myChart" ref={this.chartRef} />
-      </div>
-    );
-  }
+    componentDidUpdate() {
+        const labels = this.state.chart.data.labels;
+        const data = labels.map(
+            (x, i, a) => (this.calculateTax(x) - this.calculateTax(a[i - 1])) / 1000
+        );
+        const effectiveTaxRateData = labels.map(
+            (x, i) => this.calculateTax(x) / labels[i]
+        );
+        this.state.chart.data.datasets[0].data = effectiveTaxRateData;
+        this.state.chart.data.datasets[1].data = data;
+        this.state.chart.update("active");
+    }
+
+    render() {
+        return (
+            <div className="App App-header">
+                <LabeledSelect
+                    onChange={(event) =>
+                        this.handleStateChange("filingStatus", event.target.value)
+                    }
+                    label="Filing Status"
+                    selectOptions={Object.values(FilingStatusEnum)}
+                ></LabeledSelect>
+                <canvas id="myChart" ref={this.chartRef} />
+            </div>
+        );
+    }
 }
