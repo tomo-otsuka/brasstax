38ac4238a256a209fcf068a70009fe2ef1a00f48
Tomo Otsuka <tomo.otsuka@gmail.com>
2021-04-14 22:24:17 -0700

    use hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { TaxChart } from "./components/TaxChart.jsx";
 import {
   LabeledCheckbox,
