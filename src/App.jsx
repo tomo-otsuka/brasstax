871c6d851ebcfe4d11deae94fd11995bd0e904b8
Tomo Otsuka <tomo.otsuka@airbnb.com>
2025-07-07 11:44:14 -0700

    Feat: Add logo to navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ import { EstimatedTaxes } from "./components/EstimatedTaxes.jsx";
 import { TaxChart } from "./components/TaxChart.jsx";
 import { MarriagePenalty } from "./components/MarriagePenalty";
 import { StateTaxComparison } from "./components/StateTaxComparison.jsx";
-import { AppBar, Tabs, Tab, Container } from "@mui/material";
+import { ReactComponent as Logo } from "./brasstax-logo.svg";
+import { AppBar, Tabs, Tab, Container, Box } from "@mui/material";
 import { MonetizationOn, BarChart, People, Public } from "@mui/icons-material";
 
 function App() {
@@ -22,44 +23,48 @@ function App() {
   return (
     <>
       <AppBar position="static">
-        <Tabs
-          value={location.pathname}
-          onChange={(event, newValue) => {
-            navigate(newValue);
-          }}
-          centered
-          textColor="inherit"
-          indicatorColor="secondary"
-        >
-          <Tab
-            label="Estimated Taxes"
-            value="/brasstax/estimated-taxes"
-            component={Link}
-            to="/brasstax/estimated-taxes"
-            icon={<MonetizationOn />}
-          />
-          <Tab
-            label="Tax Chart"
-            value="/brasstax/tax-chart"
-            component={Link}
-            to="/brasstax/tax-chart"
-            icon={<BarChart />}
-          />
-          <Tab
-            label="Marriage Penalty"
-            value="/brasstax/marriage-penalty"
-            component={Link}
-            to="/brasstax/marriage-penalty"
-            icon={<People />}
-          />
-          <Tab
-            label="State Tax Comparison"
-            value="/brasstax/state-tax-comparison"
-            component={Link}
-            to="/brasstax/state-tax-comparison"
-            icon={<Public />}
-          />
-        </Tabs>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Logo style={{ height: 40, width: "auto", margin: "0 16px" }} />
+          <Tabs
+            value={location.pathname}
+            onChange={(event, newValue) => {
+              navigate(newValue);
+            }}
+            centered
+            textColor="inherit"
+            indicatorColor="secondary"
+            sx={{ flexGrow: 1 }}
+          >
+            <Tab
+              label="Estimated Taxes"
+              value="/brasstax/estimated-taxes"
+              component={Link}
+              to="/brasstax/estimated-taxes"
+              icon={<MonetizationOn />}
+            />
+            <Tab
+              label="Tax Chart"
+              value="/brasstax/tax-chart"
+              component={Link}
+              to="/brasstax/tax-chart"
+              icon={<BarChart />}
+            />
+            <Tab
+              label="Marriage Penalty"
+              value="/brasstax/marriage-penalty"
+              component={Link}
+              to="/brasstax/marriage-penalty"
+              icon={<People />}
+            />
+            <Tab
+              label="State Tax Comparison"
+              value="/brasstax/state-tax-comparison"
+              component={Link}
+              to="/brasstax/state-tax-comparison"
+              icon={<Public />}
+            />
+          </Tabs>
+        </Box>
       </AppBar>
       <Container>
         <Routes>
