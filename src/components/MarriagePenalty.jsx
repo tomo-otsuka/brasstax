f211b51c4d7348a3652dec9effa40c9ab74a8d31
Tomo Otsuka <tomo.otsuka@airbnb.com>
2025-07-07 12:25:46 -0700

    Build: Fix formatting and update build artifacts

--- src/components/MarriagePenalty.jsx
+++ src/components/MarriagePenalty.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback, useMemo } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import {
   DeductionTypeEnum,
   FilingStatusEnum,
